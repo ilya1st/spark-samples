ac93947d4eb217e38641a6c900becf7116b9eb18
Ilya V. Azarov <brainstorm@brainstorm.name>
2019-07-24 01:42:46 +0300

    sql

--- sql/src/main/java/SQL.java
+++ sql/src/main/java/SQL.java
@@ -1,17 +1,14 @@
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.apache.spark.sql.functions.col;
 
-
-public class CountFailSample {
-    private final static Logger LOGGER = LoggerFactory.getLogger(CountFailSample.class);
-    private final static String APP_NAME = "CountFailSample";
+public class SQL {
+    private final static Logger LOGGER = LoggerFactory.getLogger(SQL.class);
+    private final static String APP_NAME = "SQL";
     private final static String FILE_PATH = "examples/src/main/resources/people.json";
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName(APP_NAME);
