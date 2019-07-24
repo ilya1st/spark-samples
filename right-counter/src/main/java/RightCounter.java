7b5727671979e846f71fd5c09f74a0f3b577b0e8
Ilya V. Azarov <brainstorm@brainstorm.name>
2019-07-25 02:00:36 +0300

    report about ready

--- right-counter/src/main/java/RightCounter.java
+++ right-counter/src/main/java/RightCounter.java
@@ -4,12 +4,8 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.util.LongAccumulator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 public class RightCounter {
-    private final static Logger LOGGER = LoggerFactory.getLogger(RightCounter.class);
     private final static String APP_NAME = "RightCounter";
     private final static String FILE_PATH = "/home/brainstorm/spark-2.4.3-bin-hadoop2.7/README.md";
     public static void main(String[] args) {
--- sql/src/main/java/SQL.java
+++ sql/src/main/java/SQL.java
@@ -2,12 +2,10 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import static org.apache.spark.sql.functions.col;
 
 public class SQL {
-    private final static Logger LOGGER = LoggerFactory.getLogger(SQL.class);
     private final static String APP_NAME = "SQL";
     private final static String FILE_PATH = "examples/src/main/resources/people.json";
     public static void main(String[] args) {
@@ -17,18 +15,22 @@ public class SQL {
         df.show();
         df.printSchema();
         System.out.println("Number of partitions"+ df.rdd().partitions().length);
-        /*
         df.select("name").show();
         df.select(col("name"), col("age").plus(1)).show();
         df.filter(col("age").gt(21)).show();
         df.filter(col("age").gt(21)).show();
-        df.groupBy("age").count().show();*/
-        Dataset<Row> sqlDF = spark.sql("SELECT * FROM people");
-        sqlDF.show();
-        // Register the DataFrame as a global temporary view
-        //df.createGlobalTempView("people");
-// Global temporary view is tied to a system preserved database `global_temp`
-        //spark.sql("SELECT * FROM global_temp.people").show();
-        //spark.createDataset();
+        df.groupBy("age").count().show();
+        try {
+            df.createTempView("people");
+            System.out.println("SELECT * FROM people");
+            Dataset<Row> sqlDF = spark.sql("SELECT * FROM people");
+            sqlDF.show();
+            // Register the DataFrame as a global temporary view
+            sqlDF.createGlobalTempView("people");
+            // Global temporary view is tied to a system preserved database `global_temp`
+            spark.sql("SELECT * FROM global_temp.people").show();
+        } catch (Exception e) {
+            System.out.println("Error occured " + e);
+        }
     }
 }
